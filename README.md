715f854d67a238ed2faf5c52dc02d99e3b9e857e
SAKURA Ayame <137654183+Iris2664@users.noreply.github.com>
2024-06-30 00:30:04 +0900

    Added a warning for unsupported browsers.

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ License [SIL OPEN FONT LICENSE Version 1.1](https://github.com/adobe-fonts/sourc
 Morisawa BIZ UDGothic  
 [https://github.com/googlefonts/morisawa-biz-ud-gothic](https://github.com/googlefonts/morisawa-biz-ud-gothic)  
 License [SIL OPEN FONT LICENSE Version 1.1](https://github.com/googlefonts/morisawa-biz-ud-gothic?tab=OFL-1.1-1-ov-file)
+
+## JavaScript
+
+Platform.js v1.3.6  
+[https://github.com/bestiejs/platform.js](https://github.com/bestiejs/platform.js)  
+License [MIT license](https://github.com/bestiejs/platform.js?tab=MIT-1-ov-file#readme)
---
+++ js/incompatibility-verdict.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const browser = platform.name;
+const version = platform.version;
+
+if (browser == "Firefox" && version < "117") {
+    alert("最新版のFirefoxでないと閲覧できません.");
+}
+
+if (browser == "Chrome" && version < "112") {
+    alert("最新版のChromeでないと閲覧できません.");
+}
+
+if (browser == "Microsoft Edge" && version < "112") {
+    alert("最新版のMicrosoft Edgeでないと閲覧できません.");
+}
+
+if (browser == "IE") {
+    alert("Internet Explorerはサポートが終了しています. 他のブラウザから閲覧してください.");
+}
+
+if (browser == "Safari" && version < "16.5") {
+    alert("最新版のSafariでないと閲覧できません.");
+}
+
+if (browser == "Opera" && version < "98") {
+    alert("最新版のOperaでないと閲覧できません.");
+}
+
+console.log(browser);
+console.log(version);

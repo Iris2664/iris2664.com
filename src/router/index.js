3112b4b2280891f1806496dc531cc11c56028070
HINATA Ayame <137654183+Iris2664@users.noreply.github.com>
2024-10-28 04:10:07 +0900

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import Home from '../views/Home.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -28,6 +29,6 @@ const router = createRouter({
 
 export default new Router({
   mode: 'history',
-  base: '/iris2664.com/',
+  base: './',
   routes
 })

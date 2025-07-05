4e7fcfca86afbf2121d7a2320526f85dd71f268c
HINATA Ayame <mail@iris2664.com>
2025-07-06 01:26:11 +0900

    update

--- pages/about.vue
+++ pages/about.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="about-container">
     <div class="left">
-      <img class="profile-icon" src="@/assets/image/img-icon.webp" alt="Icon">
-      <div>
-        <h3 class="myname">HINATA Ayame</h3>
-        <p>Security Researcher / YouTuber / Programmer</p>
-      </div>
-      <div>
-        <div class="myarea">
-          <img class="icons" src="@/assets/image/map.png" alt="map">
-          <p>Chiba, Japan</p>
+      <div class="left_sticky">
+        <img class="profile-icon" src="@/assets/image/img-icon.webp" alt="Icon">
+        <div>
+          <h3 class="myname">HINATA Ayame</h3>
+          <p>Security Researcher / YouTuber / Programmer</p>
         </div>
         <div>
-          <img class="icons" src="@/assets/image/link.png" alt="link">
-          <p></p>
-        </div>
-        <div class="myarea">
-          <svg class="x" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <title>X</title>
-            <path
-              d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-          </svg>
-          <a href="http://" target="_blank" rel="noopener noreferrer">
-            @Iris2664
-          </a>
-        </div>
-        <div class="myarea">
-          <svg class="github" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <title>GitHub</title>
-            <path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-          </svg>
-          <a href="http://" target="_blank" rel="noopener noreferrer">
-            @Iris2664
-          </a>
+          <div class="myarea">
+            <img class="icons" src="@/assets/image/map.png" alt="map">
+            <p>Chiba, Japan</p>
+          </div>
+          <div>
+            <img class="icons" src="@/assets/image/link.png" alt="link">
+            <p></p>
+          </div>
+          <div class="myarea">
+            <svg class="x" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <title>X</title>
+              <path
+                d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+            </svg>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              @Iris2664
+            </a>
+          </div>
+          <div class="myarea">
+            <svg class="github" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <title>GitHub</title>
+              <path
+                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+            </svg>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              @Iris2664
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -51,6 +53,135 @@
         このサイトはNuxt 3とTypeScriptで構築されており、最新の技術を活用しています。
         ぜひ、私の活動をフォローしていただけると嬉しいです！
       </p>
+      <section>
+        <h2>About Me</h2>
+        <ul>
+          <li>Handle: 陽向 あやめ / HINATA Ayame</li>
+          <li>Gender: Female</li>
+          <li>Pronoun: she/her</li>
+          <li>Occupation: Bachelor Student (Information Technology and Business), Security Researcher, Programmer
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Research Interests</h2>
+        <ul>
+          <li>Information Law</li>
+          <li>Privacy-Enhancing Technologies</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Career</h2>
+        <ul>
+          <li>2018-04. 私立高等学校普通科（通信制） 入学</li>
+          <li>2021-03. 私立高等学校普通科（通信制） 卒業</li>
+          <li>2021-04. サイバー大学IT総合学部IT総合学科 入学</li>
+          <li>2023-04. サイバー大学IT総合学部IT総合学科 休学</li>
+          <li>2024-04. サイバー大学IT総合学部IT総合学科 復学</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Interests/Expertise</h2>
+        <section>
+          <h3>Expertise</h3>
+          <p>Information Law, System Audit, Cyber Security and Low Layer(Operating System)</p>
+        </section>
+        <section>
+          <h3>Interests</h3>
+          <ul>
+            <li>低レイヤー / オペレーティングシステム / Kernel / CPU</li>
+            <li>サイバーセキュリティ / 暗号理論 / 電子署名 / 情報法 / プライバシー</li>
+            <li>ネットワーク / サーバー構築・運用 / TCP/IP / 無線通信</li>
+            <li>人工知能 / 生成系AI / 機械学習 / 画像認識</li>
+            <li>知的財産権 / 著作権 / 特許権 / 商標権</li>
+            <li>メタバース / 電脳</li>
+          </ul>
+        </section>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <ul>
+          <li>Windows, Mac, Linux</li>
+          <li>C/C++, Rust, JavaScript, TypeScript, Python</li>
+          <li>Visual Studio Code</li>
+          <li>Git (GitHub)</li>
+          <li>Webサイト製作</li>
+          <li>基礎的なシステム監査知識</li>
+          <li>パーソナルコンピュータの組み立て（自作PC）</li>
+          <li>Premiere Proを用いた動画制作</li>
+          <li>Illustrator, Photoshop, CLIP STUDIO PAINTを用いた簡単なイラスト, 同人誌系冊子作成</li>
+          <li>プログラミング教室での講師補助，教室運営補助経験による未成年者へのプログラミング指導</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Activities</h2>
+        <section>
+          <h3>Presentationss</h3>
+          <p>準備中</p>
+        </section>
+        <section>
+          <h3>Publication</h3>
+          <p>準備中</p>
+        </section>
+        <section>
+          <h3>Products</h3>
+          <p>準備中</p>
+        </section>
+        <section>
+          <h3>Academic Papers</h3>
+          <p>準備中</p>
+        </section>
+        <section>
+          <h3>Other Activities</h3>
+          <ul>
+            <li>2024-08-26 Virtual YouTuberを始める</li>
+            <li>2024-08-31 IT系VTuberグループ「ぶいぎーく！」所属
+              <a href="https://vgeekpro.com/talents/iris2664" target="_blank" rel="noopener noreferrer">
+                陽向あやめ - ぶいぎーく！
+              </a>
+            </li>
+          </ul>
+        </section>
+      </section>
+      <section>
+        <h2>Account</h2>
+        <ul>
+          <li>X a.k.a Twitter: <a href="https://x.com/Iris2664" target="_blank" rel="noopener noreferrer">@Iris2664</a>
+          </li>
+          <li>Bluesky: <a href="https://bsky.app/profile/iris2664.com" target="_blank"
+              rel="noopener noreferrer">@iris2664.com</a></li>
+          <li>GitHub: <a href="https://github.com/Iris2664" target="_blank" rel="noopener noreferrer">@Iris2664</a>
+          </li>
+          <li>GitLab: <a href="https://gitlab.com/Iris2664" target="_blank" rel="noopener noreferrer">@Iris2664</a>
+          </li>
+          <li>YouTube: <a href="https://www.youtube.com/@iris2664jp" target="_blank"
+              rel="noopener noreferrer">@Iris2664jp</a></li>
+          <li>Twitch: <a href="https://www.twitch.tv/iris266" target="_blank" rel="noopener noreferrer">iris2664</a>
+          </li>
+          <li>Qiita: <a href="https://qiita.com/Iris2664" target="_blank" rel="noopener noreferrer">@Iris2664</a>
+          </li>
+          <li>Zenn: <a href="https://zenn.dev/lris2664" target="_blank" rel="noopener noreferrer">@Iris2664</a></li>
+          <li>Hatena Blog: <a href="https://iris2664.hatenablog.jp" target="_blank"
+              rel="noopener noreferrer">iris2664.hatenablog.jp</a></li>
+          <li>connpass: <a href="https://connpass.com/user/Iris2664/" target="_blank"
+              rel="noopener noreferrer">Iris2664</a></li>
+          <li>AtCoder: <a href="https://atcoder.jp/users/Iris2664" target="_blank"
+              rel="noopener noreferrer">Iris2664</a></li>
+          <li>VRChat: <a href="https://vrchat.com/home/user/usr_6ff9abda-9919-47eb-ba74-50e9b92f2c2a" target="_blank"
+              rel="noopener noreferrer">Hinata Ayame</a></li>
+          <li>Keybase.io: <a href="https://keybase.io/iris2664" target="_blank" rel="noopener noreferrer">iris2664</a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Contact</h2>
+        <p class="contact">ご連絡はTwitterのDMまたはメールでお願いします. </p>
+        <p class="e-mail">mail@$[THIS_DOMAIN]</p>
+        <p>※ 特定電子メールの送付はお断りします.</p>
+        <p>You can verify me at Keybase.io.</p>
+        <p>PGP public key in preparation…</p>
+        <p>Encrypted messages are most welcome.</p>
+      </section>
     </div>
   </div>
 
